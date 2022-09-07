c1bf0a982ee0ce89cd859cea16d4bb793b901e81
jimin <jimin@localhost>
2022-09-07 20:17:58 +0900

    third commit

--- WordMasterProject/src/com/mycom/word/Main.java
+++ WordMasterProject/src/com/mycom/word/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.println("Hello World! 안녕 ");
+	
 		new WordManager().start();//WordManager안에 start 함수 호출 
 	}
 }
